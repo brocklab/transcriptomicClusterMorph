f244cec9d36f4c9a3b8f93df00ae9c3f9204bc48
Tyler Jost <tyler_jost@utexas.edu>
2022-10-21 11:40:14 -0500

    Updated with object-oriented class style for image outputs

--- explore/testClassificationFull.py
+++ explore/testClassificationFull.py
@@ -4,10 +4,13 @@ sys.path.append('../scripts')
 import cellMorphHelper
 
 import os
-from skimage.io import imread
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import pickle 
+
+from skimage import measure, img_as_float
+from skimage.io import imread
 
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.data.datasets import register_coco_instances
@@ -29,46 +32,67 @@ def findFluorescenceColor(RGB, mask):
         return "red"
     else:
         return "NaN"
-# %%
-predictor = cellMorphHelper.getSegmentModel('../output/TJ2201Split16ClassifyFull', numClasses=2)
-# %%
-if 'cellMorph_test' in DatasetCatalog:
-    DatasetCatalog.remove('cellMorph_test')
-    MetadataCatalog.remove('cellMorph_test')
 
-    print('Removing testing')
-register_coco_instances("cellMorph_test", {}, '../data/cocoFiles/TJ2201Split16FullWellTest.json', "")
-# %%
-images = DatasetCatalog['cellMorph_test']()
-fileNames = [image['file_name'] for image in images]
-wells = [fileName.split('_')[1] for fileName in fileNames]
+class imgSegment:
+    """
+    Stores information about cell output from image
+    """
+    def __init__(self, fileName, predictor):
+        assert 'phaseContrast' in fileName
+        self.pcImg = fileName
+        self.compositeImg = self.pcImg.replace('phaseContrast', 'composite')
 
-imgLog = {'D2': {}, 'E2': {}, 'E7': {}}
-for image in tqdm(images):
-    fileName = image['file_name']
-    well = fileName.split('_')[1]
-    # For each well/file log the number correct and the number of cells total
-    imgLog[well][fileName] = [0, 0]
-    annotations = image['annotations']
-    if len(annotations) == 0:
-        continue
-    # Load images and find results from predictor
-    imgPc = imread(fileName)
-    imgComp = imread(fileName.replace('phaseContrast', 'composite'))
-    outputs = predictor(imgPc)
-    outputs = outputs['instances']  
-    masks = outputs.pred_masks.numpy()
-    pred_classes = outputs.pred_classes.numpy()
-    # For each mask, compare its identity to the predicted class
-    for mask, pred_class in zip(masks, pred_classes):
-        actualColor = findFluorescenceColor(imgComp.copy(), mask.copy())
-        if actualColor == 'red':
-            classification = 0
-        elif actualColor == 'green':
-            classification = 1
-        else:
-            continue
-    
-        if classification == pred_class:
-            imgLog[well][fileName][0] += 1
-        imgLog[well][fileName][1] +=1
+        self.well = self.pcImg.split('_')[1]
+
+        masks, pred_classes, actualClassifications = self.assignPhenotype(predictor)
+
+        self.masks = masks
+        self.pred_classes = pred_classes
+        self.actualClassifications = actualClassifications
+
+    def assignPhenotype(self, predictor):
+        """
+        Compares predicted and output phenotype assignments. Stores mask as well. 
+        """
+        imgPc = imread(self.pcImg)
+        imgComp = imread(self.compositeImg)
+        outputs = predictor(imgPc)['instances']
+        masks = outputs.pred_masks.numpy()
+        pred_classes = outputs.pred_classes.numpy()
+        actualClassifications, fluorescentPredClasses = [], []
+        # For each mask, compare its identity to the predicted class
+        for mask, pred_class in zip(masks, pred_classes):
+            actualColor = findFluorescenceColor(imgComp.copy(), mask.copy())
+            if actualColor == 'red':
+                classification = 0
+            elif actualColor == 'green':
+                classification = 1
+            else:
+                continue
+            actualClassifications.append(classification)
+            fluorescentPredClasses.append(pred_class)
+        return (masks, pred_classes, actualClassifications)
+
+    def imshow(self):
+        """Temp function to show image, TODO: Add labels"""
+        imgPc = imread(self.pcImg)
+        plt.imshow(imgPc)
+# %%
+writeData = 1
+
+if writeData:
+    predictor = cellMorphHelper.getSegmentModel('../output/TJ2201Split16ClassifyFull', numClasses=2)
+
+    imgLog = []
+    if 'cellMorph_test' not in DatasetCatalog:
+        register_coco_instances("cellMorph_test", {}, '../data/cocoFiles/TJ2201Split16FullWellTest.json', "")
+        images = DatasetCatalog['cellMorph_test']()
+
+    c = 0
+    # For each image, catalog model outputs and their predicted/actual classes
+    for image in tqdm(images):
+        imgLog.append(imgSegment(image['file_name'], predictor))
+        if c % 10 == 0:
+            pickle.dump(imgLog, open('../data/fullClassificationAccuracy.pickle', "wb"))
+
+        c += 1
