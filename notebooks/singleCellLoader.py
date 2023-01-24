e5baf16a7120c5736fb131b33ab735eafe37474d
Tyler Jost <tyler_jost@utexas.edu>
2023-01-23 23:28:36 -0600

    expanding segmentation to full images

--- notebooks/singleCellLoader.py
+++ notebooks/singleCellLoader.py
@@ -156,136 +156,134 @@ class singleCellCrop(Dataset):
 
         return [np.array(itm, dtype='object') for itm in list(zip(*l))]
 # %%
-if __name__ == '__main__':
-    experiment = 'TJ2201'
-    datasetDicts = np.load(f'./{experiment}DatasetDict.npy', allow_pickle=True)
-    # %%
-    mean = np.array([0.5, 0.5, 0.5])
-    std = np.array([0.25, 0.25, 0.25])
-    data_transforms = {
-        'train': transforms.Compose([
-            # transforms.RandomResizedCrop(224),
-            # transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            # transforms.Normalize(mean, std)
-        ]),
-        'test': transforms.Compose([
-            # transforms.Resize(356),
-            # transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            # transforms.Normalize(mean, std)
-        ]),
-    }
-    dataPath = f'../../data/{experiment}/{experiment}Split16/phaseContrast'
+experiment = 'TJ2201'
+datasetDicts = np.load(f'../data/{experiment}/{experiment}DatasetDict.npy', allow_pickle=True)
+mean = np.array([0.5, 0.5, 0.5])
+std = np.array([0.25, 0.25, 0.25])
+data_transforms = {
+    'train': transforms.Compose([
+        # transforms.RandomResizedCrop(224),
+        # transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        # transforms.Normalize(mean, std)
+    ]),
+    'test': transforms.Compose([
+        # transforms.Resize(356),
+        # transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        # transforms.Normalize(mean, std)
+    ]),
+}
+dataPath = f'../data/{experiment}/split16/phaseContrast'
 
-    image_datasets = {x: singleCellCrop(datasetDicts, data_transforms[x], dataPath, phase=x) 
-                        for x in ['train', 'test']}
-    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'test']}
-    dataloaders = {x: DataLoader(image_datasets[x], batch_size=4, shuffle=True)
-                        for x in ['train', 'test']}
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    # %%
-    def imshow(inp):
-        """Imshow for Tensor."""
-        inp = inp.numpy().transpose((1, 2, 0))
-        plt.figure(figsize=(15,5))
-        plt.imshow(inp)
-        plt.show()
+image_datasets = {x: singleCellCrop(datasetDicts, data_transforms[x], dataPath, phase=x) 
+                    for x in ['train', 'test']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'test']}
+dataloaders = {x: DataLoader(image_datasets[x], batch_size=4, shuffle=True)
+                    for x in ['train', 'test']}
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# %%
+def imshow(inp):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    plt.figure(figsize=(15,5))
+    plt.imshow(inp)
+    plt.show()
 
-    # Get a batch of training data
-    inputs, classes = next(iter(dataloaders['train']))
+# Get a batch of training data
+inputs, classes = next(iter(dataloaders['train']))
 
-    # Make a grid from batch
-    out = torchvision.utils.make_grid(inputs)
-    imshow(out)
-    # %%
-    def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+imshow(out)
+# %%
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
-        best_model_wts = copy.deepcopy(model.state_dict())
-        best_acc = 0.0
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
 
-        for epoch in tqdm(range(num_epochs), leave=False):
-            # print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-            # print('-' * 10)
+    for epoch in tqdm(range(num_epochs), leave=False):
+        # print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        # print('-' * 10)
 
-            # Each epoch has a training and validation phase
-            for phase in ['train', 'test']:
-                if phase == 'train':
-                    model.train()  # Set model to training mode
-                else:
-                    model.eval()   # Set model to evaluate mode
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'test']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
 
-                running_loss = 0.0
-                running_corrects = 0
+            running_loss = 0.0
+            running_corrects = 0
 
-                # Iterate over data.
-                for inputs, labels in tqdm(dataloaders[phase], position=0, leave=True):
-                    # I have no idea why you have to do this but...
-                    # https://discuss.pytorch.org/t/runtimeerror-expected-object-of-scalar-type-double-but-got-scalar-type-float-for-argument-2-weight/38961/9
-                    inputs = inputs.float()
-                    inputs = inputs.to(device)
-                    labels = labels.to(device)
+            # Iterate over data.
+            for inputs, labels in tqdm(dataloaders[phase], position=0, leave=True):
+                # I have no idea why you have to do this but...
+                # https://discuss.pytorch.org/t/runtimeerror-expected-object-of-scalar-type-double-but-got-scalar-type-float-for-argument-2-weight/38961/9
+                inputs = inputs.float()
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
-                    # forward
-                    # track history if only in train
-                    with torch.set_grad_enabled(phase == 'train'):
-                        outputs = model(inputs)
-                        _, preds = torch.max(outputs, 1)
-                        loss = criterion(outputs, labels)
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
 
-                        # backward + optimize only if in training phase
-                        if phase == 'train':
-                            optimizer.zero_grad()
-                            loss.backward()
-                            optimizer.step()
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        optimizer.zero_grad()
+                        loss.backward()
+                        optimizer.step()
 
-                    # statistics
-                    running_loss += loss.item() * inputs.size(0)
-                    running_corrects += torch.sum(preds == labels.data)
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
 
-                if phase == 'train':
-                    scheduler.step()
+            if phase == 'train':
+                scheduler.step()
 
-                epoch_loss = running_loss / dataset_sizes[phase]
-                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-                print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                    phase, epoch_loss, epoch_acc))
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
 
-                # deep copy the model
-                if phase == 'test' and epoch_acc > best_acc:
-                    best_acc = epoch_acc
-                    best_model_wts = copy.deepcopy(model.state_dict())
-            if epoch % 10 == 0:
-                torch.save(model.state_dict(), )
+            # deep copy the model
+            if phase == 'test' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+        if epoch % 10 == 0:
+            torch.save(model.state_dict(), )
 
-            print()
+        print()
 
 
 
-        # load best model weights
-        model.load_state_dict(best_model_wts)
-        return model
-    # %%
-    model = models.resnet152(pretrained=True)
-    nIncrease = image_datasets['train'].nIncrease
-    modelSaveName = f'../../models/classifySingleCellCrop{nIncrease}Resnet152.pth'
-    num_ftrs = model.fc.in_features
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+# %%
+model = models.resnet152(pretrained=True)
+nIncrease = image_datasets['train'].nIncrease
+modelSaveName = f'../../models/classifySingleCellCrop{nIncrease}Resnet152.pth'
+num_ftrs = model.fc.in_features
 
-    # Create new layer and assign in to the last layer
-    # Number of output layers is now 2 for the 2 classes
-    model.fc = nn.Linear(num_ftrs, 2)
-    model.to(device)
+# Create new layer and assign in to the last layer
+# Number of output layers is now 2 for the 2 classes
+model.fc = nn.Linear(num_ftrs, 2)
+model.to(device)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001)
 
-    # Scheduler to update lr 
-    # Every 7 epochs the learning rate is multiplied by gamma
-    setp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+# Scheduler to update lr 
+# Every 7 epochs the learning rate is multiplied by gamma
+setp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-    model = train_model(model, criterion, optimizer, setp_lr_scheduler, num_epochs=50)
-    # %%
-    device = torch.device("cuda")
-    model.to(device)
-    torch.save(model.state_dict(), '../../models/classifySplitResnet152.pth')
+model = train_model(model, criterion, optimizer, setp_lr_scheduler, num_epochs=50)
+# %%
+device = torch.device("cuda")
+model.to(device)
+torch.save(model.state_dict(), '../../models/classifySplitResnet152.pth')
