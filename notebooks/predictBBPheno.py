ab3738bc2e15e7b5ef9d06eecb836003e5a12cd4
Tyler Jost <tyler_jost@utexas.edu>
2023-02-14 19:17:57 -0600

    Only cocultured wells

--- notebooks/predictBBPheno.py
+++ notebooks/predictBBPheno.py
@@ -23,6 +23,7 @@ maxAmt      = 15000
 batch_size  = 40
 num_epochs  = 40
 modelType   = 'resnet152'
+notes = 'Run only on coculture wells'
 
 modelID, idSource = modelTools.getModelID(sys.argv)
 modelSaveName = Path(f'../models/classifySingleCellCrop-{modelID}.pth')
@@ -36,8 +37,8 @@ modelInputs = {
 'num_epochs'    : num_epochs,
 'modelType'     : modelType,
 'modelName'     : modelSaveName.parts[-1],
-'modelIDSource' : idSource
-
+'modelIDSource' : idSource,
+'notes'         : notes
 }
 
 modelTools.printModelVariables(modelInputs)
@@ -45,6 +46,8 @@ modelTools.printModelVariables(modelInputs)
 dataPath = Path(f'../data/{experiment}/raw/phaseContrast')
 datasetDictPath = Path(f'../data/{experiment}/split16/{experiment}DatasetDictNoBorder.npy')
 datasetDicts = np.load(datasetDictPath, allow_pickle=True)
+co = ['B7','B8','B9','B10','B11','C7','C8','C9','C10','C11','D7','D8','D9','D10','D11','E7','E8','E9','E10','E11']
+datasetDicts = [seg for seg in datasetDicts if seg['file_name'].split('_')[1] in co]
 # %%
 dataloaders, dataset_sizes = makeImageDatasets(datasetDicts, 
                                                dataPath, 
