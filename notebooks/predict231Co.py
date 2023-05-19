ab5891ba858c7fa86f33cb4b5662b5a19d25b71b
Tyler Jost <taj159@gmail.com>
2023-05-19 16:31:36 -0500

    Added coculture scripts

--- notebooks/predict231Co.py
+++ notebooks/predict231Co.py
@@ -21,12 +21,12 @@ import torch.optim as optim
 # %%
 experiment  = 'TJ2201'
 nIncrease   = 25
-maxAmt      = 10000
+maxAmt      = 20000
 batch_size  = 64
 num_epochs  = 32
 modelType   = 'resnet152'
 optimizer = 'sgd'
-notes = 'Full run without blackout'
+notes = 'Run on coculture wells only'
 
 modelID, idSource = modelTools.getModelID(sys.argv)
 modelSaveName = Path(f'../models/classification/classifySingleCellCrop-{modelID}.pth')
@@ -46,10 +46,53 @@ modelInputs = {
 }
 
 # %%
-
-# %%
 dataPath = Path(f'../data/{experiment}/raw/phaseContrast')
 datasetDictPath = Path(f'../data/{experiment}/split16/{experiment}DatasetDictNoBorderFull.npy')
 datasetDicts = np.load(datasetDictPath, allow_pickle=True)
 co = ['B7','B8','B9','B10','B11','C7','C8','C9','C10','C11','D7','D8','D9','D10','D11','E7','E8','E9','E10','E11']
 datasetDicts = [seg for seg in datasetDicts if seg['file_name'].split('_')[1] not in co]
+# %%
+dataloaders, dataset_sizes = makeImageDatasets(datasetDicts, 
+                                               dataPath,
+                                               modelInputs
+                                            )
+np.unique(dataloaders['train'].dataset.phenotypes, return_counts=True)
+# %%
+inputs, classes = next(iter(dataloaders['train']))
+# %%
+# plt.imshow(inputs[16].numpy().transpose((1,2,0)))
+# %%
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+if not modelSaveName.parent.exists():
+    raise NotADirectoryError('Model directory not found')
+
+model = getTFModel(modelInputs['modelType'])
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+# optimizer = optim.Adam(model.parameters(), lr=0.001)
+# %%
+model2 = nn.DataParallel(model)
+
+# %%
+modelDetailsPrint = modelTools.printModelVariables(modelInputs)
+
+with open(resultsSaveName, 'a') as file:
+    file.write(modelDetailsPrint)
+
+# Scheduler to update lr
+# Every 7 epochs the learning rate is multiplied by gamma
+setp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+model = train_model(model, 
+                    criterion, 
+                    optimizer, 
+                    setp_lr_scheduler, 
+                    dataloaders, 
+                    dataset_sizes, 
+                    modelSaveName,
+                    resultsSaveName,
+                    num_epochs=num_epochs
+                    )
--- notebooks/predict231Late.py
+++ notebooks/predict231Late.py
@@ -50,6 +50,7 @@ datasetDicts = np.load(datasetDictPath, allow_pickle=True)
 co = ['B7','B8','B9','B10','B11','C7','C8','C9','C10','C11','D7','D8','D9','D10','D11','E7','E8','E9','E10','E11']
 datasetDicts = [seg for seg in datasetDicts if seg['file_name'].split('_')[1] in co]
 datasetDicts = [record for record in datasetDicts if len(record['annotations']) > 0]
+datasetDicts = [record for record in datasetDicts if len(record['annotations']) > 0]
 
 # %% Check dates
 dates = []
