e15b3765d16bc7cded8352963f3c38eb355ce2ec
Tyler Jost <taj159@gmail.com>
2024-02-19 13:39:02 -0600

    Improved visualizations

--- src/visualization/figures/increasingBB.py
+++ src/visualization/figures/increasingBB.py
@@ -21,11 +21,12 @@ import torch
 import torch.nn.functional as F
 # %%
 homePath = Path('../../../')
-datasetDictPath = homePath / './data/TJ2201/split16/TJ2201DatasetDictNoBorder.npy'
+datasetDictPath = homePath / './data/TJ2201/split16/TJ2201DatasetDictNoBorderFull.npy'
 datasetDicts = np.load(datasetDictPath, allow_pickle=True)
 # %% Get single cell
 idx = random.randint(0,len(datasetDicts))
 # idx = 15902
+idx = 15909
 print(idx)
 c = idx
 for seg in datasetDicts[idx:]:
@@ -46,9 +47,10 @@ print(c)
 polyx = cell['segmentation'][0][0::2]
 polyy = cell['segmentation'][0][1::2]
 bb = cell['bbox']
-plt.imshow(img)
-plt.plot(polyx, polyy, 'b--', linewidth=3)
-# %% Plot only cell
+# plt.imshow(img)
+# plt.plot(polyx, polyy, 'b--', linewidth=3)
+
+# Plot only cell
 maxRows, maxCols = 150, 150
 polygon = list(zip(polyy, polyx))
 
@@ -81,27 +83,48 @@ imgPathWhole = homePath / 'data/TJ2201/raw/phaseContrast' / imgNameWhole
 imgWhole = imread(imgPathWhole)
 nIncreases = [0, 25, 65]
 increasingBB = {}
+num = 1
 for nIncrease in nIncreases:
-    imgCrop = bbIncrease(cell['segmentation'][0], cell['bbox'], seg['file_name'], imgWhole, nIncrease = nIncrease)
+    polyx = cell['segmentation'][0][0::2]
+    polyy = cell['segmentation'][0][1::2]
+    poly = np.array([polyx, polyy]).T
+    imgCrop = bbIncrease(poly, cell['bbox'], seg['file_name'], imgWhole, nIncrease = nIncrease, nIms=16)
     increasingBB[nIncrease] = imgCrop
 # 
-plt.figure(figsize=(10,10))
-
-# plt.subplot(221)
-# plt.imshow(img, cmap='gray')
-# plt.plot(polyx, polyy, 'b--', linewidth=2)
-# plt.title('No bounding box', fontsize=15)
-# plt.axis('off')
-
-c = 1
+fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+sfs = fig.subfigures(1, 3)
+c = 0
 for nIncrease in nIncreases:
-    plt.subplot(3,1,c)
-    plt.imshow(increasingBB[nIncrease], cmap='gray')
-    plt.title(f'BB Increase = {nIncrease} px', fontsize=15)
-    plt.axis('off')
+    sf = sfs[c]
+    ax = sf.add_axes([0, 0, 1, 0.85])
+    ax.imshow(increasingBB[nIncrease], cmap = 'gray')
+    sf.suptitle(f'{nIncrease} px\nIncrease', fontsize=15)
+    ax.axis('off')
     c += 1
+fig.savefig(homePath / 'figures/increasingBBDemonstration.png', dpi=600)
 
-plt.savefig(homePath / 'figures/increasingBBDemonstration.png', dpi=600)
 # %%
-cocultureModelPath = homePath / 'results/classificationTraining/classifySingleCellCrop-702500.out'
-plotTrainingRes(cocultureModelPath, title='Coculture Wells Only')
+augmentations = [None, 'blackoutCell', 'stamp']
+changingAugmentations = {}
+for augmentation in augmentations:
+    polyx = cell['segmentation'][0][0::2]
+    polyy = cell['segmentation'][0][1::2]
+    poly = np.array([polyx, polyy]).T
+    imgCrop = bbIncrease(poly, cell['bbox'], seg['file_name'], imgWhole, nIncrease = 25, nIms=16, augmentation=augmentation)
+    changingAugmentations[augmentation] = imgCrop
+# %%
+fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+sfs = fig.subfigures(1, 3)
+augmentationNameDict = {
+    None: 'No Augmentation',
+    'blackoutCell': 'No Texture',
+    'stamp': 'No Surrounding'
+}
+c = 0
+for augmentation in augmentations:
+    sf = sfs[c]
+    ax = sf.add_axes([0, 0, 1, 0.85])
+    ax.imshow(changingAugmentations[augmentation], cmap = 'gray')
+    sf.suptitle(f'{nIncrease} px\nIncrease', fontsize=15)
+    ax.axis('off')
+    c += 1
