d481ed6fb5e50be9c232535ad4083d994ba6ce84
Tyler Jost <taj159@gmail.com>
2024-04-19 16:00:06 -0500

    Added bold text

--- notebooks/publicationFigures/multipanel.py
+++ notebooks/publicationFigures/multipanel.py
@@ -9,16 +9,16 @@ increasingSubpop = imread('../../figures/publication/results/increasingBBDemonst
 subPopResults = imread('../../figures/publication/results/increasingBBSubpop.png')
 
 fig, axs = plt.subplots(2, figsize = (4, 4), height_ratios = [1.75,3], layout = 'constrained')
+trans = mtransforms.ScaledTranslation(-20/72, 7/72, fig.dpi_scale_trans)
 
 axs[0].imshow(increasingSubpop, aspect = 'auto')
 axs[0].axis('off')
-trans = mtransforms.ScaledTranslation(-20/72, 7/72, fig.dpi_scale_trans)
 axs[0].text(0.0, 1.0, 'a', transform=axs[0].transAxes + trans,
-        fontsize='large', va='top', ha = 'left')
+        fontsize='large', weight = 'bold', va='top', ha = 'left')
 axs[1].imshow(subPopResults, aspect = 'auto')
 axs[1].axis('off')
 axs[1].text(0.0, 1.0, 'b', transform=axs[1].transAxes + trans,
-        fontsize='large', va='top', ha = 'left')
+        fontsize='large', weight = 'bold', va='top', ha = 'left')
 
 fig.savefig('../../figures/publication/multipanel/increasingBB.png', 
             dpi = 500,
@@ -34,9 +34,10 @@ axs[0].imshow(augmentations, aspect = 'auto')
 axs[0].axis('off')
 trans = mtransforms.ScaledTranslation(-20/72, 7/72, fig.dpi_scale_trans)
 axs[0].text(0.0, 1.0, 'a', transform=axs[0].transAxes + trans,
-        fontsize='large', va='top', ha = 'left')
+        fontsize='large', va='top', ha = 'left', weight = 'bold')
 axs[1].imshow(augmentationResults, aspect = 'auto')
 axs[1].axis('off')
 axs[1].text(0.0, 1.0, 'b', transform=axs[1].transAxes + trans,
-        fontsize='large', va='top', ha = 'left')
+        fontsize='large', weight = 'bold', va='top', ha = 'left')
 fig.savefig('../../figures/publication/multipanel/augmentations.png', dpi = 500)
+# %%
